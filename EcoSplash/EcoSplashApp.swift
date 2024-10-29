6e430c10c9f764bfd3e31e258e760d49bbfc934f
Humberto Pérez Galindo <Humberto_1630@hotmail.com>
2024-10-28 20:51:28 -0600

    Initial Commit

---
+++ EcoSplash/EcoSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoSplashApp.swift
+//  EcoSplash
+//
+//  Created by Humberto Pérez Galindo on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
