ebd654c75372c077f019255b91bf0fc19c1199a0
Humberto Pérez Galindo <Humberto_1630@hotmail.com>
2024-11-01 10:54:18 -0600

    50% advanced

---
+++ EcoSplash/Models/Hat.swift
@@ -0,0 +1,8 @@
+//
+//  Hat.swift
+//  EcoSplash
+//
+//  Created by Humberto Pérez Galindo on 31/10/24.
+//
+
+import Foundation
---
+++ EcoSplash/Models/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  EcoSplash
+//
+//  Created by Humberto Pérez Galindo on 31/10/24.
+//
+
+import Foundation
